d53b949b1a4364fafa73fe48a074674824d948a3
mzharov <zharmiha@yandex.ru>
2019-03-15 15:52:29 +0300

    HashMap вместо LinkedList + временная версия вывода в файл

--- src/EmplCompute.java
+++ src/EmplCompute.java
@@ -8,8 +8,9 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Iterator;
-import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 
 public class EmplCompute {
@@ -17,7 +18,11 @@ public class EmplCompute {
     private String inputFile;
     private String outputFile;
     private List<EmplPerson> emplPersons = new ArrayList<>();
-    private List<Departments> departments = new LinkedList<>();
+    private Map<String, Departments> departments = new ConcurrentHashMap<>();
+
+    private final String[] tabs = {"empl_id", "empl_name" , "new_dept", "prev_dept",
+            "new_dept_avg", "prev_dept_avg",
+            "new_dept_old_avg", "prev_dept_old_avg"};
 
 
     EmplCompute(String inputFile, String outputFile) {
@@ -34,35 +39,26 @@ public class EmplCompute {
             String line = "";
             while ((line = br.readLine()) != null) {
                 String[] tmp = line.split(";");
-
                 emplPersons.add(new EmplPerson(Integer.parseInt(tmp[0].trim()),
                         tmp[1].trim(),
                         new BigDecimal(tmp[3].trim())));
-                /*
-                 * Если объект для департамента уже создан, то нового сотрудника добавляем туда
-                 */
-                Iterator<Departments> iter = departments.iterator();
-                boolean hasDept = false;
-                while (iter.hasNext()) {
-                    Departments dept = iter.next();
-                    if(dept.getDptName().equals(tmp[2].trim())) {
-                        dept.addEmpl(new EmplPerson(Integer.parseInt(tmp[0].trim()),
-                                tmp[1].trim(),
-                                new BigDecimal(tmp[3].trim())));
-                        hasDept = true;
-                        break;
-                    }
-                }
-                /*
-                 * Если департамент встчечается в первый раз,
-                 * создаем новый объект и добавляем сотрудника туда
-                 */
-                if(!hasDept) {
+                if(departments.containsKey(tmp[2].trim())) {
+                    /*
+                     * Если объект для департамента уже создан, то нового сотрудника добавляем туда
+                     */
+                    departments.get(tmp[2].trim()).addEmpl(new EmplPerson(Integer.parseInt(tmp[0].trim()),
+                            tmp[1].trim(),
+                            new BigDecimal(tmp[3].trim())));
+                } else {
+                    /*
+                     * Если департамент встчечается в первый раз,
+                     * создаем новый объект и добавляем сотрудника туда
+                     */
                     Departments dpt = new Departments(tmp[2].trim());
                     dpt.addEmpl(new EmplPerson(Integer.parseInt(tmp[0].trim()),
                             tmp[1].trim(),
                             new BigDecimal(tmp[3].trim())));
-                    departments.add(dpt);
+                    departments.putIfAbsent(tmp[2].trim(), dpt);
                 }
             }
         } catch (FileNotFoundException e) {
@@ -86,7 +82,27 @@ public class EmplCompute {
      */
 
     public void printDepartments() {
-        departments.forEach(n->System.out.println(n.toString()));
+        departments.forEach((k,v)->System.out.println(v.toString()));
+    }
+
+
+    private String getFormatter() {
+        //"%s %15s %15s %15s %-15s %-15s %-15s %-15s\r\n"
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = 0; i < tabs.length; i++) {
+            sb.append("%");
+            if(i >=1 && i <=3) {
+                sb.append("-");
+            }
+            sb.append((tabs[i].length()+1));
+            sb.append("s");
+            if(i!=tabs.length-1) {
+                sb.append(" ");
+            }
+        }
+        sb.append("\r\n");
+        return sb.toString();
     }
 
 
@@ -96,20 +112,23 @@ public class EmplCompute {
     public void computeTransactions() {
         List<String> tmpArray = new ArrayList<>();
 
-        Iterator<Departments> deptIter = departments.iterator();
+        Iterator<Map.Entry<String,Departments>> deptIter =
+                departments.entrySet().iterator();
         while (deptIter.hasNext()) {
-            Departments depts = deptIter.next();
+            Departments depts = deptIter.next().getValue();
             //Проходим по всем сотрудникам и сравниваем их зарплату с средней зарплатой по департаменту
             Iterator<EmplPerson> emplsIter = depts.getEmployeersList().iterator();
             while (emplsIter.hasNext()) {
                 EmplPerson emplPerson = emplsIter.next();
                 //Если зарплата меньше среднего, то ищем департамент, где она больше среднего
                 if(emplPerson.getSalary().compareTo(depts.getAvgSalary())  < 0) {
-                    Iterator<Departments> deptIterInner = departments.iterator();
+                    Iterator<Map.Entry<String, Departments>> deptIterInner =
+                            departments.entrySet().iterator();
                     while (deptIterInner.hasNext()) {
-                        Departments deptsInner = deptIterInner.next();
+                        Departments deptsInner = deptIterInner.next().getValue();
                         if(!depts.getDptName().equals(deptsInner.getDptName())) {
                             if(emplPerson.getSalary().compareTo(deptsInner.getAvgSalary())  > 0) {
+                                /*
                                 tmpArray.add(emplPerson.getId() + ";"
                                         + emplPerson.getLastName() + ";"
                                         + deptsInner.getDptName() + ";"
@@ -117,20 +136,31 @@ public class EmplCompute {
                                         + deptsInner.getTAvgSalary(emplPerson.getSalary()) + ";"
                                         + depts.getTAvgSalary(emplPerson.getSalary().negate()) + ";"
                                         + deptsInner.getAvgSalary() + ";"
-                                        + depts.getAvgSalary());
+                                        + depts.getAvgSalary());*/
+
+                                tmpArray.add(String.format(getFormatter(),
+                                        emplPerson.getId(),
+                                        emplPerson.getLastName(),
+                                        deptsInner.getDptName(),
+                                        depts.getDptName(),
+                                        deptsInner.getTAvgSalary(emplPerson.getSalary()),
+                                        depts.getTAvgSalary(emplPerson.getSalary().negate()),
+                                        deptsInner.getAvgSalary(),
+                                        depts.getAvgSalary()));
                             }
                         }
                     }
                 }
             }
         }
+
         //Если были найдены сотрудники, записываем полученный массив данных в выходной файл
         if(tmpArray.size() > 0) {
            try {
-               tmpArray.add(0, "//id;name;new department; previous department; "
-                       + "new department avg salary; previous department avg  salary;"
-                       + "new department old avg salary; previous department old avg salary"
-               );
+               tmpArray.add(0, String.format(getFormatter(),
+                       "empl_id", "empl_name" , "new_dept", "prev_dept",
+                       "new_dept_avg", "prev_dept_avg",
+                       "new_dept_old_avg", "prev_dept_old_avg"));
                Files.write(Paths.get(outputFile), tmpArray, Charset.defaultCharset());
                System.out.println("Данные успешно записаны в файл.");
            }
@@ -144,4 +174,5 @@ public class EmplCompute {
             System.out.println("Нет сотрудников удовлетворяющих условиям");
         }
     }
+
 }
